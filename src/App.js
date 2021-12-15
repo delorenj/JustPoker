f0f316d97253b7739e666b050f443af1888debb2
Jarad DeLorenzo <jaradd@gmail.com>
2021-12-14 19:59:47 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { PubSub } from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'us-east-1:7b6e86a1-dd25-439a-933e-968a1063543a',
+    region: 'us-east-1',
+    identityPoolRegion: 'us-east-1',
+    userPoolId: 'us-east-1_7u1mjfHck',
+    userPoolWebClientId: '5ft7l91d192vgekcim0grou0lf'
+  }
+});
+
+Amplify.addPluggable(new AWSIoTProvider({
+  aws_pubsub_region: 'us-east-1',
+  aws_pubsub_endpoint: 'wss://a2vj8u89yznrga-ats.iot.us-east-1.amazonaws.com/mqtt'
+  //aws_pubsub_endpoint: 'wss://a2vj8u89yznrga-ats.iot.us-east-1.amazonaws.com/mqtt'
+}));
+
+PubSub.subscribe('justpoker').subscribe({
+  next: data => console.log('Message received', data),
+  error: error => console.error(error),
+  close: () => console.log('Done'),
+});
+
+console.log("identityPoolId: " + process.env.REACT_APP_IDENTITY_POOL_ID)
 
 function App() {
   return (
@@ -7,17 +33,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Messages Received:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <ul>
+          <li>Check the console...</li>
+        </ul>
+      </main>
     </div>
   );
 }
